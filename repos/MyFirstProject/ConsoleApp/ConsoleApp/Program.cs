0303187c5688843a9d38ba4f01d61df983307fd6
Sherkhan <45025141+bsn-82@users.noreply.github.com>
2018-11-14 23:10:07 +0600

    Initial commit

---
+++ repos/MyFirstProject/ConsoleApp/ConsoleApp/Program.cs
@@ -0,0 +1,40 @@
+﻿//Использование пространства имен System
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp
+{
+    //Главный класс программы "ЗНАКОМСТВО"
+    class HelloWorld
+    {   
+        //Главный метод программы
+        static void Main(string[] args)
+        {
+            //Текстовая переменная
+            string name;
+            
+            //Заголовок консольного окна
+            Console.Title = "Давайте познакомимся ...";
+
+            //Сообщение в консоли
+            Console.WriteLine("Как Вас зовут?");
+
+            //Считывание текста
+            name = Console.ReadLine();
+
+            //Еще одна текстовая переменная
+            string txt;
+            txt = "Очень приятно, "+ name +"!";
+
+            //Заголовок консольного окна
+            Console.Title = "Знакомство состоялось";
+
+            //Отображение сообщения в консольном окне
+            Console.WriteLine(txt);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ repos/MyFirstProject/Listing01_03/Listing01_03/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Windows.Forms;
+using Microsoft.VisualBasic;
+
+//Класс с главным методом программы
+class DialogDemo
+{
+    //Главный метод программы
+    static void Main()
+    {
+     
+        
+     /* //Второе задание стр. 60
+        string name = Interaction.InputBox("Your FirstName", "Please, Enter your name");
+        string show, age = Interaction.InputBox("Your Age","Plesa, enter your age");
+        int ageint;
+        ageint = Int32.Parse(age);
+        show = name + " " + ageint;        
+        MessageBox.Show(show + " years old");
+    */
+
+     /* //Первое задание стр. 60
+     
+        string name, surname, show;
+        name = Interaction.InputBox("Введете свое имя");
+        surname = Interaction.InputBox("Введите свою фамилию");
+        show = name + " " + surname;
+        MessageBox.Show("Ваше полное имя " + show);
+    */
+    
+    /*  //Вычисляем возраст, парсим текст в число
+        string res, txt1;
+        int year = 2018, age, born;
+        res = Interaction.InputBox("В каком году вы родились?", "Год рождения");
+        born = Int32.Parse(res);
+        age = year - born;
+        txt1 = "Тогда вам "+ age +" лет";
+        MessageBox.Show(txt1, "Возраст");
+    */
+
+
+    /*  //Отображение диалогового окна "Знакомство".
+       
+        MessageBox.Show("Продолжаем изучать С#",        //Сообщение
+        "Изучаем С#",                                   //Заголовок
+        MessageBoxButtons.YesNoCancel,                  //Кнопки Да Нет и Отменить
+        MessageBoxIcon.Exclamation);                    //Пиктограмма   
+
+        string name;                                    //Текстовая переменная
+        name = Interaction.InputBox(                    //Отображение окна с полем ввода
+        "Как Вас зовут?",                               //Текст над полем ввода
+        "Давайте знакомится ..."                        //Название окна
+         );
+        string txt = "Очень приятно, " + name + "!";    //Еще одна переменная
+        MessageBox.Show(txt, "Знакомство состоялось");
+     */
+
+    }
+}

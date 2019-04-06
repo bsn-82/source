e005fbb5a9dfc29a85dc99b831fe19e324c36d95
Sherkhan <45025141+bsn-82@users.noreply.github.com>
2019-04-06 15:38:49 +0600

    Это первый коммит через bash

--- repos/MyFirstProject/ConsoleApp/ConsoleApp/Program.cs
+++ repos/MyFirstProject/ConsoleApp/ConsoleApp/Program.cs
@@ -7,8 +7,117 @@ using System.Threading.Tasks;
 
 namespace ConsoleApp
 {
+    class HomeWork
+    {
+        static void Main()
+        {
+        
+        /* // Складывает два числа 5+3=7 и показывает разность этих чисел 5-3=2
+            string num1_st, num2_st;
+            int num1, num2, summ;
+            Console.WriteLine("Enter first number");
+            num1_st = Console.ReadLine();
+            num1 = Int32.Parse(num1_st);
+
+            Console.WriteLine("Enter second number");
+            num2_st = Console.ReadLine();
+            num2 = Int32.Parse(num2_st);
+
+            summ = num1 + num2;
+            Console.WriteLine(summ);
+            Console.WriteLine(num1 - num2);
+
+
+        /* // Число на ед. меньше введенного, введенное число и на 1 больше
+            string num_st;
+            int num;
+            Console.WriteLine("Enter number");
+            num_st = Console.ReadLine();
+            num = Int32.Parse(num_st);
+
+            Console.WriteLine(1 - num);
+            Console.WriteLine(num);
+            Console.WriteLine(+ 1 + num);
+            
+
+            /* //Сложение двух чисел
+                string num1, num2;
+                int num1_st, num2_st, result;
+                Console.WriteLine("Enter first num");
+                num1 = Console.ReadLine();
+                num1_st = Int32.Parse(num1);
+
+                Console.WriteLine("Enter second num");
+                num2 = Console.ReadLine();
+                num2_st = Int32.Parse(num2);
+                result = num1_st + num2_st;
+                Console.WriteLine(result);
+            */
+
+            /* // По возрасту определяет год рождения
+                string name, age1;
+                int age, year = 2018, result;
+                Console.WriteLine("Enter FirstName");
+                name = Console.ReadLine();
+                Console.WriteLine("Enter age");
+                age1 = Console.ReadLine();
+                age = Int32.Parse(age1);
+                result = year - age;
+                Console.WriteLine(name + " your born " + result);
+            */
+
+            /*  //Пишет имя и счиает возрат(год рождения - текущий год)
+                string name, born;
+                int year = 2018, born1, result;
+                Console.WriteLine("Введите свое имя");
+                name = Console.ReadLine();
+                Console.WriteLine("Введите год рождения");
+                born = Console.ReadLine();
+                born1 = Int32.Parse(born);
+                result = year - born1;
+                Console.WriteLine(name + " " + result);
+            */
+
+            /* //Вычисление возраста
+                int age, year = 2018, result;
+                string age1;
+                Console.WriteLine("Введите ваш год рождения");
+                age = Console.ReadLine();
+                age = Int32.Parse(age1);            
+                result = year - age;
+                Console.WriteLine("Ваш возраст " + result);
+             */
+            //Задание 1 на стр. 60 - Выолнено.
+            /*  string name, show, age;
+                int age1;
+                Console.WriteLine("Введите Ваше имя");
+                name = Console.ReadLine();
+                Console.WriteLine("Введите Ваш возраст");
+                age = Console.ReadLine();
+                age1 = Int32.Parse(age);
+                show = name + " " + age1;
+                Console.WriteLine(show);
+            */
+
+
+            //Задание 1 на стр. 60 - Выолнено.
+            /*  string name, surname, show;
+                Console.WriteLine("Введите Ваше имя");
+                name = Console.ReadLine();
+                Console.WriteLine("Введите Вашу фамилию");
+                surname = Console.ReadLine();
+                show = name + " " + surname;
+                Console.Title = "Заголовок";
+                Console.WriteLine(show);
+            */
+
+        }
+
+    }
+
+
     //Главный класс программы "ЗНАКОМСТВО"
-    class HelloWorld
+ /*   class HelloWorld
     {   
         //Главный метод программы
         static void Main(string[] args)
@@ -37,4 +146,5 @@ namespace ConsoleApp
             Console.ReadKey();
         }
     }
+    */
 }
--- repos/MyFirstProject/Listing01_03/Listing01_03/Program.cs
+++ repos/MyFirstProject/Listing01_03/Listing01_03/Program.cs
@@ -8,52 +8,132 @@ class DialogDemo
     //Главный метод программы
     static void Main()
     {
-     
-        
-     /* //Второе задание стр. 60
-        string name = Interaction.InputBox("Your FirstName", "Please, Enter your name");
-        string show, age = Interaction.InputBox("Your Age","Plesa, enter your age");
-        int ageint;
-        ageint = Int32.Parse(age);
-        show = name + " " + ageint;        
-        MessageBox.Show(show + " years old");
+        int num1, num2;
+        string result;
+        num1 = Int32.Parse(Interaction.InputBox("Введите число", "Ввод данных"));
+        num2 = num1 / 3;
+        result = (num2 == 3 ? "Делится" : "Неделится");
+        MessageBox.Show(result);
+
+
+
+
+    /* //Складывает два числа 5 + 3 = 8 и показывает разность этих чисел 5 - 3 = 2
+        string num1 = Interaction.InputBox("Введите первое число"), num2 = Interaction.InputBox("Введите второе число");
+        int num11 = Int32.Parse(num1), num22 = Int32.Parse(num2);        
+        MessageBox.Show("Сумма двух чисел = " + (num11 + num22) + " Разность = " + (num11 - num22));        
     */
 
-     /* //Первое задание стр. 60
-     
-        string name, surname, show;
-        name = Interaction.InputBox("Введете свое имя");
-        surname = Interaction.InputBox("Введите свою фамилию");
-        show = name + " " + surname;
-        MessageBox.Show("Ваше полное имя " + show);
+    /* // вывод последовательности из трех чисел, чило меньше, число введенное и число на 1 больше.
+
+        string num1;
+        int num;
+        num1 = Interaction.InputBox("Введите число");
+        num = Int32.Parse(num1);
+        MessageBox.Show("Первое число " + (num - 1) + " Второе число " + num + " Теретье число " + (num + 1));
+    */
+        
+     /* //сумма двух введенных чисел
+        string num1, num2;
+        int result, result1, result2;
+        num1 = Interaction.InputBox("Введите первое число");
+        num2 = Interaction.InputBox("Введите второе число");
+        result1 = Int32.Parse(num1);
+        result2 = Int32.Parse(num2);
+        result = result1 + result2;              
+        MessageBox.Show("Результат" + result);
+    */   
+
+     /*   //Ввод Имя + Год рождения, Вывод Имя и возраст
+        string name, born1;
+        int year = 2018, result, born;
+        name = Interaction.InputBox("Введите Ваше имя");
+        born1 = Interaction.InputBox("Введите Ваш год рождения");
+        born = Int32.Parse(born1);
+        result = year - born;
+        MessageBox.Show("Возраст " + name + " составляет " + result + " лет");
+    */
+
+
+    /* //По году рождения определяется возраст
+        string name, born;
+        int year = 2018, result;
+        name = Interaction.InputBox("Введите ваше имя");
+        born = Interaction.InputBox("Введите год рождения");
+        result = Int32.Parse(born);
+        int Age = year - result;
+        MessageBox.Show("Возрат " + name + " составляет " + Age);        
     */
     
-    /*  //Вычисляем возраст, парсим текст в число
-        string res, txt1;
-        int year = 2018, age, born;
-        res = Interaction.InputBox("В каком году вы родились?", "Год рождения");
-        born = Int32.Parse(res);
-        age = year - born;
-        txt1 = "Тогда вам "+ age +" лет";
-        MessageBox.Show(txt1, "Возраст");
+    /*  //Выводит месяц и день, если ввести
+        *string month, days;
+        month = Interaction.InputBox("Введите месяц");
+        int month2;
+        month2 = Int32.Parse(month);
+        if (month2 == 11){
+            month = "Ноябрь";
+        }
+        else
+        {
+            MessageBox.Show("Это не ноябрь");
+        }
+
+        days = Interaction.InputBox("Введите количество дней");
+        MessageBox.Show(month + " " + days);
     */
 
+    /* //Выводит день недели, дату и месяц
+        string week, month, day, result;
+        week = Interaction.InputBox("Week", "Please, Enter week");
+        month = Interaction.InputBox("Month", "Please, Enter month" );
+        day = Interaction.InputBox("Day", "Please, Enter day");
+        MessageBox.Show(week + "/" + day + "/" + month);
+    */
 
-    /*  //Отображение диалогового окна "Знакомство".
-       
-        MessageBox.Show("Продолжаем изучать С#",        //Сообщение
-        "Изучаем С#",                                   //Заголовок
-        MessageBoxButtons.YesNoCancel,                  //Кнопки Да Нет и Отменить
-        MessageBoxIcon.Exclamation);                    //Пиктограмма   
+        /* //Второе задание стр. 60
+           string name = Interaction.InputBox("Your FirstName", "Please, Enter your name");
+           string show, age = Interaction.InputBox("Your Age","Plesa, enter your age");
+           int ageint;
+           ageint = Int32.Parse(age);
+           show = name + " " + ageint;        
+           MessageBox.Show(show + " years old");
+       */
 
-        string name;                                    //Текстовая переменная
-        name = Interaction.InputBox(                    //Отображение окна с полем ввода
-        "Как Вас зовут?",                               //Текст над полем ввода
-        "Давайте знакомится ..."                        //Название окна
-         );
-        string txt = "Очень приятно, " + name + "!";    //Еще одна переменная
-        MessageBox.Show(txt, "Знакомство состоялось");
-     */
+        /* //Первое задание стр. 60
+
+           string name, surname, show;
+           name = Interaction.InputBox("Введете свое имя");
+           surname = Interaction.InputBox("Введите свою фамилию");
+           show = name + " " + surname;
+           MessageBox.Show("Ваше полное имя " + show);
+       */
+
+        /*  //Вычисляем возраст, парсим текст в число
+            string res, txt1;
+            int year = 2018, age, born;
+            res = Interaction.InputBox("В каком году вы родились?", "Год рождения");
+            born = Int32.Parse(res);
+            age = year - born;
+            txt1 = "Тогда вам "+ age +" лет";
+            MessageBox.Show(txt1, "Возраст");
+        */
+
+
+        /*  //Отображение диалогового окна "Знакомство".
+
+            MessageBox.Show("Продолжаем изучать С#",        //Сообщение
+            "Изучаем С#",                                   //Заголовок
+            MessageBoxButtons.YesNoCancel,                  //Кнопки Да Нет и Отменить
+            MessageBoxIcon.Exclamation);                    //Пиктограмма   
+
+            string name;                                    //Текстовая переменная
+            name = Interaction.InputBox(                    //Отображение окна с полем ввода
+            "Как Вас зовут?",                               //Текст над полем ввода
+            "Давайте знакомится ..."                        //Название окна
+             );
+            string txt = "Очень приятно, " + name + "!";    //Еще одна переменная
+            MessageBox.Show(txt, "Знакомство состоялось");
+         */
 
     }
 }
